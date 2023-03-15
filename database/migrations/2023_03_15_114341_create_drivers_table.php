87b978f71852bdbf26cd1675b052e5c0b5b9c6cb
hitesh <hiteshr@zignuts.com>
2023-03-15 19:40:20 +0530

    migration modified and driver_name,vehicle_id foreigh key, deleted_at column added

---
+++ database/migrations/2023_03_15_114341_create_drivers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->string('driver_name')->nullable(false);
+            $table->foreignId('vehicle_id')->constrained('vehicles')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+    }
+};
