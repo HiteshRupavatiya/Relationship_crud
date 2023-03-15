72e6dfa968db85ce09ecd1cc0f9877e65535d96c
hitesh <hiteshr@zignuts.com>
2023-03-15 12:33:34 +0530

    added controller group routes with prefix for VoteController

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
-<?php
+    <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\VoteController;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\Route;
 
-/*
+    /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(VoteController::class)->prefix('vote')->group(function () {
+    Route::get('list', 'list')->name('vote.list');
+    Route::post('create', 'create')->name('vote.create');
+    Route::get('get/{id}', 'get')->name('vote.get');
+    Route::put('update/{id}', 'update')->name('vote.update');
+    Route::delete('delete/{id}', 'delete')->name('vote.delete');
 });
