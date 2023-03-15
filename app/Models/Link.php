170be48e582c0a01863850f7d851420d1a2444b8
hitesh <hiteshr@zignuts.com>
2023-03-15 14:41:04 +0530

    added belongsTo inverse relationship with  property

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Link extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'url',
+        'description',
+        'link_list_id',
+    ];
+
+    public function link_list()
+    {
+        return $this->belongsTo(LinkList::class);
+    }
 }
