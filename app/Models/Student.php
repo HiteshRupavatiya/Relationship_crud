7872888545abf42c5e43f9cb66b639b94d9b100a
hitesh <hiteshr@zignuts.com>
2023-03-16 12:19:42 +0530

    added with() method in Student model for getting school data

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -16,6 +16,6 @@ class Student extends Model
 
     public function standard()
     {
-        return $this->belongsTo(Standard::class);
+        return $this->belongsTo(Standard::class)->with('school');
     }
 }
