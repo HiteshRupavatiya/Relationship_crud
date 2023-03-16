d00101a0ebc8401d81934a1b4a04085226223408
hitesh <hiteshr@zignuts.com>
2023-03-16 12:44:23 +0530

    roles migration modified and role_name column, deleted_at column added

--- database/migrations/2023_03_16_070927_create_roles_table.php
+++ database/migrations/2023_03_16_070927_create_roles_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('role_name')->nullable(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
