3786d5584d9b4fa12f88147e04dbb4c16ca68e75
hitesh <hiteshr@zignuts.com>
2023-03-16 12:18:25 +0530

    removed unique() method on standard_name column

--- database/migrations/2023_03_16_044734_create_standards_table.php
+++ database/migrations/2023_03_16_044734_create_standards_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('standards', function (Blueprint $table) {
             $table->id();
-            $table->string('standard_name')->unique()->nullable(false);
+            $table->string('standard_name')->nullable(false);
             $table->foreignId('school_id')->constrained('schools')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
             $table->softDeletes();
