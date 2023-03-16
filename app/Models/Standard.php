619d5bfeb0709371d29ef51d4c73b9dba9682534
hitesh <hiteshr@zignuts.com>
2023-03-16 10:50:45 +0530

    relationship belongsTo , hasMany added

--- app/Models/Standard.php
+++ app/Models/Standard.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Standard extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'standard_name',
+        'school_id'
+    ];
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
 }
