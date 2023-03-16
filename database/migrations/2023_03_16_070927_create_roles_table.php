292ab255ddd7cce51f652a5165dc0744a3ca7880
hitesh <hiteshr@zignuts.com>
2023-03-16 12:51:11 +0530

    added unique() method on role_name column

--- database/migrations/2023_03_16_070927_create_roles_table.php
+++ database/migrations/2023_03_16_070927_create_roles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('role_name')->nullable(false);
+            $table->string('role_name')->unique()->nullable(false);
             $table->timestamps();
             $table->softDeletes();
         });
