981ac3d2fabaea85550c7ce175e5a633bc210494
hitesh <hiteshr@zignuts.com>
2023-03-16 15:35:17 +0530

    profiles migration modified and image_path,profileable, profileable_type column added

--- database/migrations/2023_03_16_100105_create_profiles_table.php
+++ database/migrations/2023_03_16_100105_create_profiles_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('image_path');
+            $table->integer('profileable');
+            $table->string('profileable_type');
             $table->timestamps();
         });
     }
