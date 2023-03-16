1e8c6511f9c2df39cbef46e48979a0e64f8e91c3
hitesh <hiteshr@zignuts.com>
2023-03-16 10:49:51 +0530

    relationship added hasManyThrough and hasMany

--- app/Models/School.php
+++ app/Models/School.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class School extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'school_name',
+    ];
+
+    public function schools()
+    {
+        return $this->hasManyThrough(Student::class, Standard::class);
+    }
+
+    public function standards()
+    {
+        return $this->hasMany(Standard::class);
+    }
 }
