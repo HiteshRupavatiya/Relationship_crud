419147a0001d8bb28841102cfacd8f963a17e569
hitesh <hiteshr@zignuts.com>
2023-03-16 10:51:15 +0530

    relationship belongsTo added

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'student_name',
+        'standard_id'
+    ];
+
+    public function standard()
+    {
+        return $this->belongsTo(Standard::class);
+    }
 }
