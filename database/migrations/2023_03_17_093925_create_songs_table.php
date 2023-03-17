4158568a9620fb3cff63f8d9dd4135f94ed75619
hitesh <hiteshr@zignuts.com>
2023-03-17 15:25:48 +0530

    column added song_name, description, deleted_at

--- database/migrations/2023_03_17_093925_create_songs_table.php
+++ database/migrations/2023_03_17_093925_create_songs_table.php
@@ -15,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('songs', function (Blueprint $table) {
             $table->id();
+            $table->string('song_name')->unique()->nullable(false);
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
