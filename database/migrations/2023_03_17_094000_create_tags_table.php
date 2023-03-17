6f02ac47b996e86db47e2a8d9af16482dc0c9b0c
hitesh <hiteshr@zignuts.com>
2023-03-17 15:26:25 +0530

    column added tag_name, deleted_at

--- database/migrations/2023_03_17_094000_create_tags_table.php
+++ database/migrations/2023_03_17_094000_create_tags_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('tag_name')->unique()->nullable(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
