77a8b15070dba943961a9fadf2be151ca8659db6
hitesh <hiteshr@zignuts.com>
2023-03-17 15:29:20 +0530

    column added tag_id, taggable_id, taggable_type, deleted_at

--- database/migrations/2023_03_17_094131_create_taggables_table.php
+++ database/migrations/2023_03_17_094131_create_taggables_table.php
@@ -14,8 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('taggables', function (Blueprint $table) {
-            $table->id();
+            $table->integer('tag_id');
+            $table->unsignedBigInteger('taggable_id');
+            $table->string('taggable_type');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
